84e5aca8ff7387ce6db887cb0b18f30098a2358b
Cristian Perez <cperezlecaros@gmail.com>
2026-04-30 10:30:34 -0700

    feat: add navigation buttons on customers table

--- app/(rs)/customers/CustomerTable.tsx
+++ app/(rs)/customers/CustomerTable.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { selectCustomerSchema } from "@/zod-schemas/customer";
+import Link from "next/link";
+import z from "zod";
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
   useReactTable,
+  CellContext,
 } from "@tanstack/react-table";
 import {
   Table,
@@ -14,19 +16,76 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useRouter } from "next/navigation";
-import z from "zod";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal, TableOfContents } from "lucide-react";
+import { selectCustomerSchema } from "@/zod-schemas/customer";
 
 type Props = {
   data: z.infer<typeof selectCustomerSchema>[];
 };
 
 const CustomerTable = ({ data }: Props) => {
-  const router = useRouter();
   const columnHelper =
     createColumnHelper<z.infer<typeof selectCustomerSchema>>();
 
+  const ActionsCell = ({
+    row,
+  }: CellContext<z.infer<typeof selectCustomerSchema>, unknown>) => {
+    return (
+      <DropdownMenuGroup>
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            render={
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Abrir menú</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            }
+          ></DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link
+                prefetch={false}
+                className="w-full"
+                href={`/tickets/form?customerId=${row.original.id}`}
+              >
+                Nuevo ticket
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link
+                prefetch={false}
+                className="w-full"
+                href={`/customers/form?customerId=${row.original.id}`}
+              >
+                Editar cliente
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </DropdownMenuGroup>
+    );
+  };
+
+  ActionsCell.displayName = "ActionsCell";
+
   const columns = [
+    columnHelper.display({
+      id: "actions",
+      header: () => <TableOfContents />,
+      cell: ActionsCell,
+    }),
     columnHelper.accessor("firstName", {
       header: "Nombre",
     }),
@@ -58,8 +117,13 @@ const CustomerTable = ({ data }: Props) => {
           {table.getHeaderGroups().map((hg) => (
             <TableRow key={hg.id}>
               {hg.headers.map((header) => (
-                <TableHead key={header.id} className="bg-secondary">
-                  <div>
+                <TableHead
+                  key={header.id}
+                  className={`bg-secondary ${header.id === "actions" ? "w-12" : ""}`}
+                >
+                  <div
+                    className={`${header.id === "actions" ? "flex justify-center items-center" : ""}`}
+                  >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -77,9 +141,6 @@ const CustomerTable = ({ data }: Props) => {
             <TableRow
               key={row.id}
               className="cursor-pointer hover:bg-border/25 dark:hover:bg-ring/40"
-              onClick={() =>
-                router.push(`/customers/form?customerId=${row.original.id}`)
-              }
             >
               {row.getVisibleCells().map((cell) => (
                 <TableCell key={cell.id} className="border">
